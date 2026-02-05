000ec2ed95dc40cbb1faf480ee0b7c34bbebe34b
Edouardmnt <edouardmenut04@gmail.com>
2026-02-05 11:05:37 +0100

    1.1 et 1.2

--- lab/src/controllers/user.js
+++ lab/src/controllers/user.js
@@ -1,23 +1,42 @@
-const db = require('../dbClient')
+const db = require('../dbClient');
 
 module.exports = {
   create: (user, callback) => {
     // Check parameters
-    if(!user.username)
-      return callback(new Error("Wrong user parameters"), null)
+    if (!user.username)
+      return callback(new Error("Wrong user parameters"), null);
+
     // Create User schema
     const userObj = {
       firstname: user.firstname,
       lastname: user.lastname,
-    }
-    // Save to DB
-    // TODO check if user already exists
-    db.hmset(user.username, userObj, (err, res) => {
-      if (err) return callback(err, null)
-      callback(null, res) // Return callback
-    })
+    };
+
+    // Check if user already exists
+    db.exists(user.username, (err, exists) => {
+      if (err) return callback(err, null);
+      if (exists) return callback(new Error("User already exists"), null);
+
+      // Save to DB
+      db.hmset(user.username, userObj, (err, res) => {
+        if (err) return callback(err, null);
+        callback(null, res); // Return callback
+      });
+    });
   },
-  // get: (username, callback) => {
-  //   // TODO create this method
-  // }
-}
+
+  get: (username, callback) => {
+    if (!username) return callback(new Error("Username required"), null);
+
+    db.exists(username, (err, exists) => {
+      if (err) return callback(err, null);
+      if (!exists) return callback(new Error("User not found"), null);
+
+      db.hgetall(username, (err, user) => {
+        if (err) return callback(err, null);
+        // Return the full user object with username
+        callback(null, { username, ...user });
+      });
+    });
+  }
+};
--- lab/test/user.controller.js
+++ lab/test/user.controller.js
@@ -1,13 +1,13 @@
-const { expect } = require('chai')
-const userController = require('../src/controllers/user')
-const db = require('../src/dbClient')
+const { expect } = require('chai');
+const userController = require('../src/controllers/user');
+const db = require('../src/dbClient');
 
 describe('User', () => {
   
   beforeEach(() => {
     // Clean DB before each test
-    db.flushdb()
-  })
+    db.flushdb();
+  });
 
   describe('Create', () => {
 
@@ -16,46 +16,59 @@ describe('User', () => {
         username: 'sergkudinov',
         firstname: 'Sergei',
         lastname: 'Kudinov'
-      }
+      };
       userController.create(user, (err, result) => {
-        expect(err).to.be.equal(null)
-        expect(result).to.be.equal('OK')
-        done()
-      })
-    })
+        expect(err).to.be.equal(null);
+        expect(result).to.be.equal('OK');
+        done();
+      });
+    });
 
     it('passing wrong user parameters', (done) => {
       const user = {
         firstname: 'Sergei',
         lastname: 'Kudinov'
-      }
+      };
       userController.create(user, (err, result) => {
-        expect(err).to.not.be.equal(null)
-        expect(result).to.be.equal(null)
-        done()
-      })
-    })
+        expect(err).to.not.be.equal(null);
+        expect(result).to.be.equal(null);
+        done();
+      });
+    });
 
-    // it('avoid creating an existing user', (done)=> {
-    //   // TODO create this test
-    //   // Warning: the user already exists
-    //   done()
-    // })
-  })
+  });
 
-  // TODO Create test for the get method
-  // describe('Get', ()=> {
-  //   
-  //   it('get a user by username', (done) => {
-  //     // 1. First, create a user to make this unit test independent from the others
-  //     // 2. Then, check if the result of the get method is correct
-  //     done()
-  //   })
-  //
-  //   it('cannot get a user when it does not exist', (done) => {
-  //     // Chech with any invalid user
-  //     done()
-  //   })
-  //
-  // })
-})
+  describe('Get', () => {
+
+    it('get a user by username', (done) => {
+      const user = {
+        username: 'sergkudinov',
+        firstname: 'Sergei',
+        lastname: 'Kudinov'
+      };
+
+      // 1. Create user first
+      userController.create(user, (err, result) => {
+        expect(err).to.be.equal(null);
+        expect(result).to.be.equal('OK');
+
+        // 2. Then get user
+        userController.get(user.username, (err, result) => {
+          expect(err).to.be.equal(null);
+          expect(result).to.deep.equal(user);
+          done();
+        });
+      });
+    });
+
+    it('cannot get a user when it does not exist', (done) => {
+      userController.get('unknownuser', (err, result) => {
+        expect(err).to.not.be.equal(null);
+        expect(result).to.be.equal(null);
+        done();
+      });
+    });
+
+  });
+
+});
