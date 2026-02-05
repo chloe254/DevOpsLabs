47f34b4d501027d72ba266b585306189a7125452
Edouardmnt <edouardmenut04@gmail.com>
2026-02-05 11:10:24 +0100

    test unitaire pour la route user

--- lab/src/routes/user.js
+++ lab/src/routes/user.js
@@ -1,29 +1,40 @@
-const express = require('express')
-const userController = require('../controllers/user')
+const express = require('express');
+const userController = require('../controllers/user');
 
-const userRouter = express.Router()
+const userRouter = express.Router();
 
-userRouter
-  .post('/', (req, resp) => {
-    userController.create(req.body, (err, res) => {
-      let respObj
-      if(err) {
-        respObj = {
-          status: "error",
-          msg: err.message
-        }
-        return resp.status(400).json(respObj)
-      }
+// POST /user
+userRouter.post('/', (req, resp) => {
+  userController.create(req.body, (err, res) => {
+    let respObj;
+    if (err) {
       respObj = {
-        status: "success",
-        msg: res
-      }
-      resp.status(201).json(respObj)
-    })
-  })
-  // .get('/:username', (req, resp, next) => { // Express URL params - https://expressjs.com/en/guide/routing.html
-  //   // TODO Create get method API
-  //   const username = req.params.username
-  // })
-  
-module.exports = userRouter
+        status: "error",
+        msg: err.message
+      };
+      return resp.status(400).json(respObj);
+    }
+    respObj = {
+      status: "success",
+      msg: res
+    };
+    resp.status(201).json(respObj);
+  });
+});
+
+// GET /user/:username
+userRouter.get('/:username', (req, resp) => {
+  const username = req.params.username;
+
+  userController.get(username, (err, user) => {
+    if (err) {
+      return resp.status(404).json({
+        status: "error",
+        message: err.message
+      });
+    }
+    resp.status(200).json(user);
+  });
+});
+
+module.exports = userRouter;
--- lab/test/user.router.js
+++ lab/test/user.router.js
@@ -1,21 +1,21 @@
-const app = require('../src/index')
-const chai = require('chai')
-const chaiHttp = require('chai-http')
-const db = require('../src/dbClient')
+const app = require('../src/index');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const db = require('../src/dbClient');
 
-chai.use(chaiHttp)
+chai.use(chaiHttp);
 
 describe('User REST API', () => {
-  
-    beforeEach(() => {
-      // Clean DB before each test
-      db.flushdb()
-    })
-    
-    after(() => {
-      app.close()
-      db.quit()
-    })
+
+  beforeEach(() => {
+    // Clean DB before each test
+    db.flushdb();
+  });
+
+  after(() => {
+    app.close();
+    db.quit();
+  });
 
   describe('POST /user', () => {
 
@@ -24,42 +24,88 @@ describe('User REST API', () => {
         username: 'sergkudinov',
         firstname: 'Sergei',
         lastname: 'Kudinov'
-      }
+      };
       chai.request(app)
         .post('/user')
         .send(user)
         .then((res) => {
-          chai.expect(res).to.have.status(201)
-          chai.expect(res.body.status).to.equal('success')
-          chai.expect(res).to.be.json
-          done()
+          chai.expect(res).to.have.status(201);
+          chai.expect(res.body.status).to.equal('success');
+          chai.expect(res).to.be.json;
+          done();
         })
         .catch((err) => {
-           throw err
-        })
-    })
-    
+          throw err;
+        });
+    });
+
     it('pass wrong parameters', (done) => {
       const user = {
         firstname: 'Sergei',
         lastname: 'Kudinov'
-      }
+      };
       chai.request(app)
         .post('/user')
         .send(user)
         .then((res) => {
-          chai.expect(res).to.have.status(400)
-          chai.expect(res.body.status).to.equal('error')
-          chai.expect(res).to.be.json
-          done()
+          chai.expect(res).to.have.status(400);
+          chai.expect(res.body.status).to.equal('error');
+          chai.expect(res).to.be.json;
+          done();
         })
         .catch((err) => {
-           throw err
-        })
-    })
-  })
+          throw err;
+        });
+    });
 
-  // describe('GET /user', ()=> {
-  //   // TODO Create test for the get method
-  // })
-})
+  });
+
+  describe('GET /user/:username', () => {
+
+    it('successfully get user', (done) => {
+      const user = {
+        username: 'sergkudinov',
+        firstname: 'Sergei',
+        lastname: 'Kudinov'
+      };
+
+      // 1. Create user first
+      chai.request(app)
+        .post('/user')
+        .send(user)
+        .then(() => {
+          // 2. Then get user
+          chai.request(app)
+            .get(`/user/${user.username}`)
+            .then((res) => {
+              chai.expect(res).to.have.status(200);
+              chai.expect(res.body.username).to.equal(user.username);
+              chai.expect(res.body.firstname).to.equal(user.firstname);
+              chai.expect(res.body.lastname).to.equal(user.lastname);
+              chai.expect(res).to.be.json;
+              done();
+            });
+        })
+        .catch((err) => {
+          throw err;
+        });
+    });
+
+    it('cannot get a user when it does not exist', (done) => {
+      chai.request(app)
+        .get('/user/unknownuser')
+        .then((res) => {
+          chai.expect(res).to.have.status(404);
+          chai.expect(res.body.status).to.equal('error');
+          chai.expect(res.body.message).to.equal('User not found');
+          chai.expect(res).to.be.json;
+          done();
+        })
+        .catch((err) => {
+          throw err;
+        });
+    });
+
+  });
+
+});
